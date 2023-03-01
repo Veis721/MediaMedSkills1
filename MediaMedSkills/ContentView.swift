2dd8c6758ed49601f9765dde338ec1040d8c93e0
User1 <user1@madskill.ru>
2023-03-01 08:34:46 +0500

    Initial Commit

---
+++ MediaMedSkills/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MediaMedSkills
+//
+//  Created by bnkwsr1 on 01.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MediaMedSkills/MediaMedSkillsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaMedSkillsApp.swift
+//  MediaMedSkills
+//
+//  Created by bnkwsr1 on 01.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct MediaMedSkillsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
